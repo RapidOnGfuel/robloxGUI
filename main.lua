c7fc1d11bb0e0571a863ae84e50c089d63e5cea0
Wyatt Caldwell <73676960+RapidOnGfuel@users.noreply.github.com>
2022-10-26 12:11:29 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,4 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RapidOnGfuel/robloxGUI/main/source.lua"))()
+local Window = Library.CreateLib("Rapid Hub", "DarkTheme")
+local Tab = Window:NewTab("Phantom Forces")
+local Section = Tab:NewSection("Main")
